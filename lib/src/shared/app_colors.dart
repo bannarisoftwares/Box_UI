c85d755cbaf0501157e1fe0d195040065c280c44
Natheesh <56362164+natheeshsunway@users.noreply.github.com>
2021-08-29 04:44:36 -0700

    Added secondary color

--- lib/src/shared/app_colors.dart
+++ lib/src/shared/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color kcPrimaryColor = Color(0xff22215B);
+const Color kcSecondaryColor = Color(0xff8E97FD);
 const Color kcMediumGreyColor = Color(0xff868686);
 const Color kcLightGreyColor = Color(0xffe5e5e5);
 const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
