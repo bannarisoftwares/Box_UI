22c7b92a1824e7fcddaf16c7288d5c6dbd2f6c95
Natheeshkumar <natheeshece@gmail.com>
2021-09-19 19:57:25 +0530

    Updated border color

--- lib/src/shared/app_colors.dart
+++ lib/src/shared/app_colors.dart
@@ -7,4 +7,4 @@ const Color kcMediumGreyColor = Color(0xff868686);
 const Color kcLightGreyColor = Color(0xffe5e5e5);
 const Color kcGreyColor = Color(0xFFF2F3F7);
 const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
-const Color kcBorderColor = Color(0xFFEEF2FE);
+const Color kcBorderColor = Color(0xFFE6E7F2);
